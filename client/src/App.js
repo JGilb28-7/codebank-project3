730a3ba4e263e527549185da631f14e6f68c6170
ala decoste <alshareefialaa@gmail.com>
2021-03-04 21:08:32 -0500

    Added passport, signupForm, login, and edited routes.

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,6 @@
-import React, { useState } from "react";
+import React, { Component } from "react";
+import axios from 'axios';
+import { Route } from 'react-router-dom';
 /*import Jumbotron from "./components/Jumbotron";
 import Nav from "./components/Nav";
 import Input from "./components/Input";
@@ -6,27 +8,72 @@ import Button from "./components/Button";
 import API from "./utils/API";
 import { RecipeList, RecipeListItem } from "./components/RecipeList";
 import { Container, Row, Col } from "./components/Grid";*/
+import Signup from './components/Signup'
+import LoginForm from './components/login-form'
 
 
-function App() {
+class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      loggedIn: false,
+      username: null
+    }
+
+    this.getUser = this.getUser.bind(this)
+    this.componentDidMount = this.componentDidMount.bind(this)
+    this.updateUser = this.updateUser.bind(this)
+  }
+
+  componentDidMount() {
+    this.getUser()
+  }
+
+  updateUser (userObject) {
+    this.setState(userObject)
+  }
+
+getUser() {
+    axios.get('/user/').then(response => {
+      console.log('Get user response: ')
+      console.log(response.data)
+      if (response.data.user) {
+        console.log('Get User: There is a user saved in the server session: ')
+
+        this.setState({
+          loggedIn: true,
+          username: response.data.user.username
+        })
+      } else {
+        console.log('Get user: no user');
+        this.setState({
+          loggedIn: false,
+          username: null
+        })
+      }
+    })
+  }
+
+  render() {
   return (
     <div className="App">
-      <header className="App-header">
-      
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    
+
+        <Route
+        path="/login"
+        render={() =>
+            <LoginForm
+            updateUser={this.updateUser}
+            />}
+        />
+        <Route
+        path="/signup"
+        render={() =>
+            <Signup/>}
+        />
+      </div>
   );
+}
 }
 
 export default App;
