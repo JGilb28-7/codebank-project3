6a4635c2276217fa9033ed3c1ab1000e14459307
James Coughlin <mrjacoughlin@gmail.com>
2021-03-06 14:03:23 -0500

    Manager-view, employee signup form.

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
-import axios from 'axios';
-import { Route } from 'react-router-dom';
+import axios from "axios";
+import { Route, Switch } from "react-router-dom";
 /*import Jumbotron from "./components/Jumbotron";
 import Nav from "./components/Nav";
 import Input from "./components/Input";
@@ -8,72 +8,70 @@ import Button from "./components/Button";
 import API from "./utils/API";
 import { RecipeList, RecipeListItem } from "./components/RecipeList";
 import { Container, Row, Col } from "./components/Grid";*/
-import Signup from './components/Signup'
-import LoginForm from './components/login-form'
-
+import Signup from "./components/Signup";
+import LoginForm from "./components/login-form";
+import { BrowserRouter } from "react-router-dom";
+import Employees from "./Pages/Employees";
 
 class App extends Component {
   constructor() {
-    super()
+    super();
     this.state = {
       loggedIn: false,
-      username: null
-    }
+      username: null,
+    };
 
-    this.getUser = this.getUser.bind(this)
-    this.componentDidMount = this.componentDidMount.bind(this)
-    this.updateUser = this.updateUser.bind(this)
+    this.getUser = this.getUser.bind(this);
+    this.componentDidMount = this.componentDidMount.bind(this);
+    this.updateUser = this.updateUser.bind(this);
   }
 
   componentDidMount() {
-    this.getUser()
+    this.getUser();
   }
 
-  updateUser (userObject) {
-    this.setState(userObject)
+  updateUser(userObject) {
+    this.setState(userObject);
   }
 
-getUser() {
-    axios.get('/user/').then(response => {
-      console.log('Get user response: ')
-      console.log(response.data)
+  getUser() {
+    axios.get("/user/").then((response) => {
+      console.log("Get user response: ");
+      console.log(response.data);
       if (response.data.user) {
-        console.log('Get User: There is a user saved in the server session: ')
+        console.log("Get User: There is a user saved in the server session: ");
 
         this.setState({
           loggedIn: true,
-          username: response.data.user.username
-        })
+          username: response.data.user.username,
+        });
       } else {
-        console.log('Get user: no user');
+        console.log("Get user: no user");
         this.setState({
           loggedIn: false,
-          username: null
-        })
+          username: null,
+        });
       }
-    })
+    });
   }
 
   render() {
-  return (
-    <div className="App">
-    
+    return (
+      <div className="App">
+        <BrowserRouter>
+          <Switch>
+            <Route
+              path="/login"
+              render={() => <LoginForm updateUser={this.updateUser} />}
+            />
 
-        <Route
-        path="/login"
-        render={() =>
-            <LoginForm
-            updateUser={this.updateUser}
-            />}
-        />
-        <Route
-        path="/signup"
-        render={() =>
-            <Signup/>}
-        />
+            <Route path="/employees" render={() => <Employees />} />
+            <Route path="/signup" render={() => <Signup />} />
+          </Switch>
+        </BrowserRouter>
       </div>
-  );
-}
+    );
+  }
 }
 
 export default App;
--- client/src/index.js
+++ client/src/index.js
@@ -1,18 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import { BrowserRouter } from 'react-router-dom';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { BrowserRouter } from "react-router-dom";
 
-import 'spectre.css/dist/spectre.min.css'
-import 'spectre.css/dist/spectre.min.css'
+import "spectre.css/dist/spectre.min.css";
+import "spectre.css/dist/spectre.min.css";
 
 ReactDOM.render(
-<BrowserRouter>
-		<App />
-	</BrowserRouter>,
-  document.getElementById('root')
+  <App />,
+
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
