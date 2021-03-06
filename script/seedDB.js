f1f352f96b4573249fa48634d49b382b5e96cf9b
James Coughlin <mrjacoughlin@gmail.com>
2021-03-06 11:44:17 -0500

    Updated SeedDb

--- script/seedDB.js
+++ script/seedDB.js
@@ -3,56 +3,138 @@ const db = require("../database/models/user");
 
 // This file empties the Books collection and inserts the books below
 
-mongoose.connect(
-  process.env.MONGODB_URI || "mongodb://localhost/codebank"
-);
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/codebank");
 const userSeed = [
-    {
-        firstName: "Imogen",
-        lastName: "Edwards",
-        phoneNumber:"(179)-998-3204",
-        email:"imogen.edwards@example.com",
-        username:"silversnake277",
-        password:"clifford",
-        date: new Date(Date.now())
-      },
-      {
-        firstName: "Jessica",
-        lastName: "Torres",
-        phoneNumber:"016974 94194",
-        email:"jessica.torres@example.com",
-        username:"2lazyelephant670",
-        password:"2ticklish",
-        date: new Date(Date.now())
-      },
-      {
-        firstName: "Imogen",
-        lastName: "Edwards",
-        phoneNumber:"(179)-998-3204",
-        email:"2imogen.edwards@example.com",
-        username:"2silversnake277",
-        password:"2clifford",
-        date: new Date(Date.now())
-      },
-      {
-        firstName: "Jessica",
-        lastName: "Gorres",
-        phoneNumber:"016974 94194",
-        email:"3jessica.gorres@example.com",
-        username:"3lazyelephant670",
-        password:"3ticklish",
-        date: new Date(Date.now())
-      },
-      ]
-   db.User.remove({})
+  {
+    firstName: "Imogen",
+    lastName: "Edwards",
+    role: "Employee",
+    phoneNumber: "179-998-3204",
+    email: "imogen.edwards@example.com",
+    age: 49,
+    gender: "female",
+    userName: "silversnake277",
+    password: "clifford",
+    date: new Date(Date.now()),
+  },
+  {
+    firstName: "Melvin ",
+    lastName: "Smith",
+    role: "Employee",
+    phoneNumber: "179-998-3204",
+    email: "melvin.edwards@example.com",
+    age: 49,
+    gender: "male",
+    userName: "blacksnake277",
+    password: "password",
+    date: new Date(Date.now()),
+  },
+  {
+    firstName: "Anthony",
+    lastName: "Edwards",
+    role: "Employee",
+    phoneNumber: "179-998-3204",
+    email: "imogen.edwards@example.com",
+    age: 49,
+    gender: "male",
+    userName: "silversnake277",
+    password: "clifford",
+    date: new Date(Date.now()),
+  },
+  {
+    firstName: "Frank",
+    lastName: "Edwards",
+    role: "Employee",
+    phoneNumber: "179-998-3204",
+    email: "imogen.edwards@example.com",
+    age: 49,
+    gender: "male",
+    userName: "snake2778",
+    password: "tommytom",
+    date: new Date(Date.now()),
+  },
+  {
+    firstName: "Charlene ",
+    lastName: "Edwards",
+    role: "Employee",
+    phoneNumber: "179-998-3204",
+    email: "charlene.steeves@example.com",
+    age: 49,
+    gender: "female",
+    userName: "silver123",
+    password: "silver123",
+    date: new Date(Date.now()),
+  },
+  {
+    firstName: "Cathy",
+    lastName: "Neal",
+    role: "Customer",
+    phoneNumber: "179-998-3204",
+    email: "cathy.neal@example.com",
+    age: 49,
+    gender: "female",
+    userName: "UserName",
+    password: "clifford",
+    date: new Date(Date.now()),
+  },
+  {
+    firstName: "Kathrine ",
+    lastName: "Lawson",
+    role: "Customer",
+    phoneNumber: "179-998-3204",
+    email: "kathrine.edwards@example.com",
+    age: 49,
+    gender: "female",
+    userName: "missLawson189",
+    password: "password",
+    date: new Date(Date.now()),
+  },
+  {
+    firstName: "Joe;",
+    lastName: "Pena",
+    role: "Customer",
+    phoneNumber: "179-998-3204",
+    email: "joe.edwards@example.com",
+    age: 49,
+    gender: "male",
+    userName: "silver277",
+    password: "clifford",
+    date: new Date(Date.now()),
+  },
+  {
+    firstName: "Allan",
+    lastName: "Fox",
+    role: "Customer",
+    phoneNumber: "179-998-3204",
+    email: "allan.edwards@example.com",
+    age: 49,
+    gender: "male",
+    userName: "snake2778",
+    password: "tommytom",
+    date: new Date(Date.now()),
+  },
+  {
+    firstName: "Deann ",
+    lastName: "Ford",
+    role: "Customer",
+    phoneNumber: "179-998-3204",
+    email: "deann.steeves@example.com",
+    age: 49,
+    gender: "female",
+    userName: "moosetracks456",
+    password: "deannFord",
+    date: new Date(Date.now()),
+  },
+];
+db.User.remove({})
   .then(() => db.User.insertMany(userSeed))
-  .then(data => {
+  .then((data) => {
     console.log(data.result.n + " records inserted!");
     process.exit(0);
   })
-  .catch(err => {
+  .catch((err) => {
     console.error(err);
     process.exit(1);
   });
 
-  module.exports = mongoose.connection;
+module.exports = mongoose.connection;
