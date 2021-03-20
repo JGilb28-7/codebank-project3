07db578ce42a6ced1b8af237d33eab2d09065d1a
Imprudent Friend <ImprudentFriend@gmail.com>
2021-03-20 00:08:24 -0400

    finished about page

--- client/src/Pages/About.js
+++ client/src/Pages/About.js
@@ -1,6 +1,8 @@
 import React from "react";
 import Nav from "../components/Nav/NavBar";
 import Footer from '../components/Footer/index'
+import moneyPic from '../assets/Screenshot 2021-03-19 235532.png'
+import '../components/Style/About.css'
 
 function About () {
     return (
@@ -8,12 +10,29 @@ function About () {
     <div>
          <Nav />
 
-         <h1>JJWA was started by an amazing brillant and wicked smart 
-             financial pros and full stack developers. 
+            <div class='containter'>
+                <div class='row'>
+                    <div class='col-md-6 animate__animated animate__slideInUp'>
+                        <img 
+                            class="img-fluid padding-btm" 
+                            src={moneyPic} 
+                            alt="money"
+                        />  
+                    </div>
+                    <div class='col-md-6 animate__animated animate__slideInDown'>
+                       <h1 class='welcome'>Welcome!</h1>
+                       <p class='about'>
+                            <span><a href='team'>JJWA </a></span>was started by amazing, brillant, and wicked smart 
+                            financial pros and full stack developers. <br></br><br></br>
              
-             Our goal is to provide customers with the simpilest way to 
-             bank, save thier $$$ and transact in a way that makes 
-             life easier for </h1>
+                            Our goal is to provide customers with the simpilest way to 
+                            bank, save thier $$$ and transact in a way that makes 
+                            life easier for everyone!
+                        </p> 
+                    </div>
+                </div>
+            </div>
+
 
         <Footer />
 
